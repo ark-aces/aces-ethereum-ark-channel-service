59572d9dddaf8edc03ece8ed88732a321ab6d993
Brady Olsen <bradyaolsen@gmail.com>
2018-01-03 18:18:46 -0800

    remove adjustment param

--- src/main/java/com/arkaces/btc_ark_channel_service/exchange_rate/ExchangeRateService.java
+++ src/main/java/com/arkaces/btc_ark_channel_service/exchange_rate/ExchangeRateService.java
@@ -1,7 +1,5 @@
 package com.arkaces.btc_ark_channel_service.exchange_rate;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -12,18 +10,14 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ExchangeRateService
 {
-    private final BigDecimal arkPerEthAdjustment;
-
     private final RestTemplate restTemplate = new RestTemplateBuilder()
         .rootUri("https://min-api.cryptocompare.com/data/")
         .build();
 
     public BigDecimal getRate(String fromCurrencyCode, String toCurrencyCode) {
-        // todo: we should sanity check the exchange rate to prevent a bad rate being used
-        BigDecimal rate = restTemplate
+        return restTemplate
             .exchange(
                 "/price?fsym={from}&tsyms={to}",
                 HttpMethod.GET,
@@ -34,7 +28,5 @@ public class ExchangeRateService
             )
             .getBody()
             .get(toCurrencyCode);
-
-        return rate.divide(arkPerEthAdjustment, BigDecimal.ROUND_HALF_UP);
     }
 }
